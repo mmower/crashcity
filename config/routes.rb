dcaf4fa186e18618c445dcfbb8894632dd66e0a8
Matt Mower <self@mattmower.com>
2008-12-27 20:56:41 +0000

    Added version resource.

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 ActionController::Routing::Routes.draw do |map|
+  map.resources :versions
+
   map.resources :applications
 
   # The priority is based upon order of creation: first created -> highest priority.
