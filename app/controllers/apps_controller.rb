5999e349ffc9e97c0a34a4dfb69e68c72b3ea46f
Matt Mower <self@mattmower.com>
2008-12-27 23:38:23 +0000

    Application list, linking to bundle crashes.

--- app/controllers/apps_controller.rb
+++ app/controllers/apps_controller.rb
@@ -1,2 +1,30 @@
 class AppsController < ApplicationController
+  def index
+    @apps = App.find( :all )
+
+    respond_to do |format|
+      format.html # index.html.erb
+      format.xml  { render :xml => @apps }
+    end
+  end
+
+  def show
+    @app = App.find( params[:id] )
+
+    respond_to do |format|
+      format.html # show.html.erb
+      format.xml  { render :xml => @app }
+    end
+  end
+
+  def destroy
+    @app = App.find( params[:id] )
+    @app.destroy
+
+    respond_to do |format|
+      format.html { redirect_to( admin_apps_url ) }
+      format.xml  { head :ok }
+    end
+  end
+  
 end
--- app/models/app.rb
+++ app/models/app.rb
@@ -1,3 +1,5 @@
 class App < ActiveRecord::Base
+  validates_uniqueness_of :bundle
+  
   has_many :crashes, :dependent => :destroy
 end
