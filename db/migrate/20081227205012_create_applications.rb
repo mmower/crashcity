cf64baad24d1c73d3e85adaebad0d12a9c9e80b2
Matt Mower <self@mattmower.com>
2008-12-27 21:16:53 +0000

    Added comment resource, updated definitions of other resources

--- db/migrate/20081227205012_create_applications.rb
+++ db/migrate/20081227205012_create_applications.rb
@@ -1,8 +1,8 @@
 class CreateApplications < ActiveRecord::Migration
   def self.up
     create_table :applications do |t|
-      t.string :process
-      t.string :bundle
+      t.string :process, :limit => 32
+      t.string :bundle, :limit => 32
       t.timestamps
     end
   end
--- db/migrate/20081227205656_create_crashes.rb
+++ db/migrate/20081227205656_create_crashes.rb
@@ -2,8 +2,9 @@ class CreateCrashes < ActiveRecord::Migration
   def self.up
     create_table :crashes do |t|
       t.integer :application_id
-      t.string :version
-      t.string :code
+      t.string :uuid, :limit => 32
+      t.string :version, :limit => 16
+      t.string :code, :limit => 8
       t.string :path
       t.timestamps
     end
