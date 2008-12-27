aa44b601e7960335e55d25b638a47d1f10a53611
Matt Mower <self@mattmower.com>
2008-12-27 23:53:11 +0000

    Counter cache crashes.

--- app/models/crash.rb
+++ app/models/crash.rb
@@ -1,5 +1,5 @@
 class Crash < ActiveRecord::Base
-  belongs_to :app
+  belongs_to :app, :counter_cache => true
   has_one :report, :dependent => :destroy
   has_many :comments, :dependent => :destroy
 end
--- db/migrate/20081227205012_create_apps.rb
+++ db/migrate/20081227205012_create_apps.rb
@@ -2,6 +2,7 @@ class CreateApps < ActiveRecord::Migration
   def self.up
     create_table :apps do |t|
       t.string :bundle, :limit => 32
+      t.integer :crashes_count, :defaut => 0
       t.timestamps
     end
   end
